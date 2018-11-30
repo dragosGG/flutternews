7af6a86123fbd515294acfe6d635cdc2712b7834
Dragos <dragos.giugula@gmail.com>
2018-11-30 14:08:50 +0100

    adding root

--- news/lib/src/resources/news_api_provider.dart
+++ news/lib/src/resources/news_api_provider.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart'show Client;
 
-
+final _root = 'https://hacker-news.firebaseio.com/v0';
 class NewsApiProvider{
 Client client = Client(); 
 
   fetchTopIds() async {
-  final response = await client.get('https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty');
+  final response = await client.get('$_root/topstories.json');
   final ids = json.decode(response.body);
   return ids;
   }
 
-  fetchItem() {
-
-  }
+  fetchItem(int id)async {
+    final response  = await client.get('$_root/item/$id.json');
+    }
 }
