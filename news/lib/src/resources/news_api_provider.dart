ea2f1d31d0b29a0dc34144efdf88662a5d64a6ae
Dragos <dragos.giugula@gmail.com>
2018-11-30 14:11:59 +0100

    return data

--- news/lib/src/resources/news_api_provider.dart
+++ news/lib/src/resources/news_api_provider.dart
@@ -13,5 +13,8 @@ Client client = Client();
 
   fetchItem(int id)async {
     final response  = await client.get('$_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson); 
     }
 }
