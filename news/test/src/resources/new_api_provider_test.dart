16d74d00e2b9949d0dd9829c3343dbbc6c379a21
Dragos <dragos.giugula@gmail.com>
2018-12-01 12:22:49 +0100

    test

--- news/test/src/resources/new_api_provider_test.dart
+++ news/test/src/resources/new_api_provider_test.dart
@@ -9,13 +9,15 @@ import 'package:http/testing.dart';
 void main() {
   test('Fetch Top Ids returns a lost of IDs',() {
   //setup test case 
-final sum = 1 +3;
+  final newsApi = NewsApiProvider();
   //expectation 
-  expect(sum, );
+  MockClinet((request) async {
+    return Response(json.encode([1,2,3,4]),200);
 
 
   });
 
 
 
+  });
 }
