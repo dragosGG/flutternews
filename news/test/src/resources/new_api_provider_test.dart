ac9f1e7aa375a768a06de517dddf236c6166c004
Dragos <dragos.giugula@gmail.com>
2018-12-01 12:21:21 +0100

    testing playing with

--- news/test/src/resources/new_api_provider_test.dart
+++ news/test/src/resources/new_api_provider_test.dart
@@ -3,3 +3,19 @@ import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+
+
+
+void main() {
+  test('Fetch Top Ids returns a lost of IDs',() {
+  //setup test case 
+final sum = 1 +3;
+  //expectation 
+  expect(sum, );
+
+
+  });
+
+
+
+}
