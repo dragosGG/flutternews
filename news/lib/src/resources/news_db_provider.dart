65da1ce5e000aa1be7bde9a5faa1a68fe17a85db
Dragos <dragos.giugula@gmail.com>
2018-12-03 18:35:45 +0100

    modeling DB

---
+++ news/lib/src/resources/news_db_provider.dart
@@ -0,0 +1,44 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+
+
+  init() async {
+    Directory documentDirectory = await.getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "items.pub");
+    db = await openDatabase(
+      path,
+      version: 1, 
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+         CREATE TABLE Items
+         (
+           id INTEGER PRIMARU KEY,
+           type TEXT,
+           by TEXT, 
+           time INTEGER, 
+           text TEXT, 
+           partent INTEGER,
+           kids BLOB,
+           dead INTEGER, 0 === false, 1 === true,
+           deleted INTEGER,
+           url TEXT,
+           title TEXT, 
+           descendants INTEGER
+         )
+         """
+
+        );
+      }
+    );
+    
+
+  }
+}
