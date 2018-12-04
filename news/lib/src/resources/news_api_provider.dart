fc52338aa3394ec4fcc1c42c701f4973cd0df5e2
Dragos <dragos.giugula@gmail.com>
2018-12-04 13:46:00 +0100

    annotating return types

--- news/lib/src/resources/news_api_provider.dart
+++ news/lib/src/resources/news_api_provider.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart'show Client;
+import "dart:async";
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
 class NewsApiProvider{
 Client client = Client(); 
 
-  fetchTopIds() async {
+  Future<List<int>>fetchTopIds() async {
   final response = await client.get('$_root/topstories.json');
   final ids = json.decode(response.body);
   return ids;
   }
 
-  fetchItem(int id)async {
+  Future<ItemModel>fetchItem(int id)async {
     final response  = await client.get('$_root/item/$id.json');
     final parsedJson = json.decode(response.body);
 
